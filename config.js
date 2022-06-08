d83bdfd31a1822e48ac5783b3321963165d836e1
takiet1 <88774597+takiet1@users.noreply.github.com>
2022-06-08 15:26:47 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    introTitle: 'Em yêu à!',
+    introDesc: `Anh biết anh sai rồi mà TT 
+    tha lỗi cho anh đi `,
+    btnIntro: 'huhu',
+    title: 'Em sẽ tha lỗi cho anh chứ? ',
+    desc: 'em mà kh tha lỗi là anh đi chết đấy huhu ',
+    btnYes: 'Tha lỗi cho anh đó <33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'vậy là em tha lỗi cho anh hả ',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Đúng rồi đóa ,yêu anh lắm <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối nay gặp mua kẹo cho em nha.',
+    btnAccept: 'Okiiiii lun   <3',
+    messLink: 'http://messenger.com/kietpro1012'
+}
